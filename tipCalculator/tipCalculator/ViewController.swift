3d59f1272c27957b8f1b520c8e28cd30f8e3c37d
Auriles El Haddad <auriles0404@gmail.com>
2016-12-15 16:22:44 +0100

    TipCalculator : Design de la vue

---
+++ tipCalculator/tipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tipCalculator
+//
+//  Created by Auriles El Haddad on 15/12/2016.
+//  Copyright © 2016 Auriles El Haddad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
