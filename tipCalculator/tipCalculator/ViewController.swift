c25357b215bfb0d38f52bbdeb7d8f67c43feb481
Auriles El Haddad <auriles0404@gmail.com>
2016-12-27 17:12:06 +0100

    Ajout de la fonction touchesBegan()

--- tipCalculator/tipCalculator/ViewController.swift
+++ tipCalculator/tipCalculator/ViewController.swift
@@ -68,7 +68,11 @@ class ViewController: UIViewController {
         
     }
     
-    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+    }
     
 }
 
