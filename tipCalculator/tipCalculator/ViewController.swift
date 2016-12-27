4292b77db52d68a6bee322259d6410a77636eb15
Auriles El Haddad <auriles0404@gmail.com>
2016-12-27 16:23:09 +0100

    Segmented Control

--- tipCalculator/tipCalculator/ViewController.swift
+++ tipCalculator/tipCalculator/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var pourcentagePourboire:Int?
+    
     @IBOutlet weak var montantFactureTextField: UITextField!
     
     @IBOutlet weak var pourboireSegmentedControl: UISegmentedControl!
@@ -19,19 +21,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var montantTotalLabel: UILabel!
     
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        pourboireSegmentedControl.addTarget(self, action: #selector(ViewController.calculerPourboire), for: UIControlEvents.valueChanged)
-    }
-
-    func calculerPourboire() {
-        print(montantFactureTextField.text!)
+        pourboireSegmentedControl.addTarget(self, action: #selector(ViewController.calculerPourcentage), for: UIControlEvents.valueChanged)
     }
     
+    func calculerPourcentage() {
+        
+        switch pourboireSegmentedControl.selectedSegmentIndex {
+            
+        case 0:
+            print("segment sélectionné : \(pourboireSegmentedControl.selectedSegmentIndex)")
+            pourcentagePourboire = 15
+            
+        case 1:
+            print("segment sélectionné : \(pourboireSegmentedControl.selectedSegmentIndex)")
+            pourcentagePourboire = 20
+            
+        case 2:
+            print("segment sélectionné : \(pourboireSegmentedControl.selectedSegmentIndex)")
+            pourcentagePourboire = 25
+            
+        default:
+            print("Aucune sélection")
+        }
+        
+        calculerPourboire(pourcent: pourcentagePourboire!)
+        
+    }
     
+    func calculerPourboire(pourcent:Int) {
+        
+        let pourboire = Int(montantFactureTextField.text!)! * pourcent / 100
+        
+        print("montant \(pourboire) euros")
+        print("montant total à payer = \(pourboire + Int(montantFactureTextField.text!)!) euros")
+        
+    }
     
     
     
