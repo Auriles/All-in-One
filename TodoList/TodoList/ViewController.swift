7fc77a92661db0fcdf0d1804490f86535ec90f9d
Auriles El Haddad <auriles0404@gmail.com>
2016-12-29 14:40:53 +0100

    TodoList - Table View Controller

---
+++ TodoList/TodoList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Auriles El Haddad on 29/12/2016.
+//  Copyright © 2016 Auriles El Haddad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
