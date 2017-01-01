fb7945be22a5a32e3f18b40e4ff0e6d43d61e3a4
Auriles El Haddad <auriles0404@gmail.com>
2017-01-01 15:42:20 +0100

    TaskGestion - Vue ajouter Todo

--- TaskGestion/TaskGestion/ViewController.swift
+++ TaskGestion/TaskGestion/ViewController.swift
@@ -9,6 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Outlets
+    
+    @IBOutlet weak var dateTextField: UITextField!
+    
+    @IBOutlet weak var todoTextField: UITextField!
+    
+    @IBOutlet weak var ajouterBtn: UIBarButtonItem!
+    
+    // Actions
+    
+    @IBAction func annuler(_ sender: UIBarButtonItem) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func ajouterTodo(_ sender: UIBarButtonItem) {
+        
+        ajouterTodoAFaire(todo: todoTextField!.text!, date: Date())
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func ajouterTodoAFaire(todo:String, date:Date) {
+        
+        print("\(date): à faire: \(todo)")
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
