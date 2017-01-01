a53e134318b1c28d0cfa7a24b6adad03997e39f2
Auriles El Haddad <auriles0404@gmail.com>
2017-01-01 15:10:06 +0100

    TaskGestion - Table View Controller

---
+++ TaskGestion/TaskGestion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaskGestion
+//
+//  Created by Auriles El Haddad on 01/01/2017.
+//  Copyright © 2017 Auriles El Haddad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
