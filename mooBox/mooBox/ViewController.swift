201bfd18a1652bb325f6b6313086abf74a8dd8fd
Auriles El Haddad <auriles0404@gmail.com>
2017-01-11 16:36:33 +0100

    Moo Box - Ajout du son avec AVAudioPlayer

--- mooBox/mooBox/ViewController.swift
+++ mooBox/mooBox/ViewController.swift
@@ -7,23 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var mooBoxImageView: UIImageView!
     
+    var player:AVAudioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        sonDeVache()
     }
 
     override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
         
         if event?.subtype == UIEventSubtype.motionShake {
-            print("shaked")
+            player.play()
         }
     }
 
 
+    func sonDeVache() {
+        
+        let vache = Bundle.main.path(forResource: "cow", ofType: "mp3")
+        
+        do {
+            try player = AVAudioPlayer(contentsOf: URL(string: vache!)!)
+        } catch {
+                print("erreur lecture mp3")
+        }
+    }
+    
 }
 
