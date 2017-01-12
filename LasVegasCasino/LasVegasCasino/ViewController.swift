5d65c8ae2f0ad6342e6c1c33a3be7417f471b1b0
Auriles El Haddad <auriles0404@gmail.com>
2017-01-12 16:17:14 +0100

    Las Vegas Casino - Picker View

--- LasVegasCasino/LasVegasCasino/ViewController.swift
+++ LasVegasCasino/LasVegasCasino/ViewController.swift
@@ -8,26 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
+    // MARK - Outlets
     @IBOutlet weak var lasVegasLabel: UILabel!
     
+    @IBOutlet weak var casinoRoulettePickerView: UIPickerView!
     
     
     
     
-
+    // MARK - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         
         lasVegasLabel.text = "Las Vegas \nCasino"
+        casinoRoulettePickerView.dataSource = self
+        casinoRoulettePickerView.delegate = self
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK - UIPickerViewDataSource
+    @available(iOS 2.0, *)
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
     }
+    
+    
+    // MARK - UIPickerViewDataSource
+    @available(iOS 2.0, *)
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 10
+    }
+
+    
 
 
 }
